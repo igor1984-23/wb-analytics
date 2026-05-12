c230f180e61b6bc3bfe83b3b8f44451c422abfd1
igor1984-23 <iarafailov@bk.ru>
2026-05-12 14:32:16 +0300

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ VALID_PASSWORD = "secret123"
 ADMIN_USERNAME = "admin"
 
 # ВАША РЕАЛЬНАЯ ССЫЛКА
-BASE_URL = "https://https://wb-analytics.streamlit.app"
+BASE_URL = "https://BASE_URL = "https://wb-analytics-mqxvuxfayhh5h5s3nqbq3ti.streamlit.app""
 
 SMTP_SERVER = "smtp.mail.ru"
 SMTP_PORT = 587
